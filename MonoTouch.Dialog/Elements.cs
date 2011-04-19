70ec88c08badfbbd6f0414cf937f1540852550e9
Robin Galloway-Lunn <robingl@aecher.com>
2011-04-20 00:33:08 +0100

    Made the cell detail label's background color clear so that subtitles will always take on the styling of the main cell.

--- MonoTouch.Dialog/Elements.cs
+++ MonoTouch.Dialog/Elements.cs
@@ -757,7 +757,10 @@ namespace MonoTouch.Dialog
 				imgView.Image = img;
 				
 				if (cell.DetailTextLabel != null)
+				{
 					cell.DetailTextLabel.TextColor = extraInfo.DetailColor ?? UIColor.Black;
+					cell.DetailTextLabel.BackgroundColor = UIColor.Clear;
+				}
 			}
 		}	
 	
@@ -765,6 +768,9 @@ namespace MonoTouch.Dialog
 		{
 			cell.BackgroundColor = UIColor.White;
 			cell.TextLabel.BackgroundColor = UIColor.Clear;
+			
+			if (cell.DetailTextLabel != null)
+				cell.DetailTextLabel.BackgroundColor = UIColor.Clear;
 		}
 
 		void IColorizeBackground.WillDisplay (UITableView tableView, UITableViewCell cell, NSIndexPath indexPath)
