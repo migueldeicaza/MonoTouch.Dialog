ed0730cbe1245f0547d783af556ecc1feb78884f
Miguel de Icaza <miguel@gnome.org>
2011-08-24 12:02:23 -0400

    Do not hardcode the width when computing the size, to support device rotation

--- MonoTouch.Dialog/Elements/ElementBadge.cs
+++ MonoTouch.Dialog/Elements/ElementBadge.cs
@@ -98,7 +98,7 @@ namespace MonoTouch.Dialog
 
 		public float GetHeight (UITableView tableView, NSIndexPath indexPath)
 		{
-			SizeF size = new SizeF (280, float.MaxValue);
+			SizeF size = new SizeF (tableView.Bounds.Width - 40, float.MaxValue);
 			float height = tableView.StringSize (Caption, Font, size, LineBreakMode).Height + 10;
 			
 			// Image is 57 pixels tall, add some padding
