8a89b6462fa8f09ce1a733a2552a215910f1c482
Alxandr <alxandr@alxandr.me>
2012-02-06 21:11:26 +0100

    Added RowBadgeElement and samples

--- MonoTouch.Dialog/Elements.cs
+++ MonoTouch.Dialog/Elements.cs
@@ -22,6 +22,7 @@ using MonoTouch.CoreGraphics;
 using System.Drawing;
 using MonoTouch.Foundation;
 using MonoTouch.Dialog.Utilities;
+using MonoTouch.CoreAnimation;
 
 namespace MonoTouch.Dialog
 {
@@ -2758,5 +2759,222 @@ namespace MonoTouch.Dialog
 			TableView.ReloadRows (new NSIndexPath [] { path }, animation);
 		}
 		
+	}
+	
+	/// <summary>
+	/// Row badge element. Ported from https://github.com/tmdvs/TDBadgedCell
+	/// </summary>
+	public class RowBadgeElement : Element
+	{
+		static NSString skey = new NSString ("BadgeElement");
+		
+		private string badgeValue;
+		
+		public UIColor Color { get; set; }
+		public UIColor HighlightColor { get; set; }
+		public float Radius { get; set; }
+		
+		
+		public RowBadgeElement (string caption) : base(caption)
+		{
+			HighlightColor = UIColor.FromRGBA (1f, 1f, 1f, 1f);
+			Color = UIColor.FromRGBA (0.530f, 0.600f, 0.738f, 1f);
+			Radius = 4f;
+		}
+		
+		public RowBadgeElement (string caption, string badgeValue) : this(caption)
+		{
+			this.badgeValue = badgeValue;
+		}
+		
+		public RowBadgeElement (string caption, NSAction tapped) : this(caption)
+		{
+			Tapped += tapped;
+		}
+		
+		public RowBadgeElement (string caption, string badgeValue, NSAction tapped) : this(caption, badgeValue)
+		{
+			Tapped += tapped;
+		}
+		
+		public event NSAction Tapped;
+		
+		public override UITableViewCell GetCell (UITableView tv)
+		{
+			BadgeCell cell = (BadgeCell)tv.DequeueReusableCell (skey);
+			if (cell == null) {
+				cell = new BadgeCell (skey);
+				cell.SelectionStyle = (Tapped != null) ? UITableViewCellSelectionStyle.Blue : UITableViewCellSelectionStyle.None;
+			}
+			cell.Accessory = UITableViewCellAccessory.None;
+			cell.TextLabel.Text = Caption;
+			cell.TextLabel.TextAlignment = UITextAlignment.Left;
+			cell.BadgeText = badgeValue;
+			cell.Color = Color;
+			cell.Radius = Radius;
+			cell.HighlightColor = HighlightColor;
+			cell.SetNeedsLayout ();
+			
+			return cell;
+		}
+		
+		public override string Summary ()
+		{
+			return Caption;
+		}
+		
+		public override void Selected (DialogViewController dvc, UITableView tableView, NSIndexPath indexPath)
+		{
+			if (Tapped != null)
+				Tapped ();
+			tableView.DeselectRow (indexPath, true);
+		}
+		
+		public override bool Matches (string text)
+		{
+			return (badgeValue != null ? badgeValue.IndexOf (text, StringComparison.CurrentCultureIgnoreCase) != -1 : false) || base.Matches (text);
+		}
+		
+		private class BadgeView : UIView
+		{
+			static float fontSize = 11;
+			
+			public string Text;
+			public float Radius = 4f;
+			public BadgeCell Parent;
+			public UIColor HighlightColor = UIColor.FromRGBA (1f, 1f, 1f, 1f);
+			public UIColor Color = UIColor.FromRGBA (0.530f, 0.600f, 0.738f, 1f);
+			public bool Shadow;
+			
+			public BadgeView (RectangleF rect) : base(rect)
+			{
+				BackgroundColor = UIColor.Clear;
+			}
+			
+			public override void Draw (RectangleF rect)
+			{
+				var fontSize = BadgeView.fontSize;
+				var text = new NSString (Text);
+				var numberSize = text.StringSize (UIFont.BoldSystemFontOfSize (fontSize));
+				var radius = Radius;
+				
+				var bounds = new RectangleF (PointF.Empty, new SizeF (numberSize.Width + 12f, 18f));
+				UIColor color;
+				if (Parent.SelectionStyle != UITableViewCellSelectionStyle.None && (Parent.Highlighted || Parent.Selected)) {
+					color = HighlightColor;
+				} else {
+					color = Color;
+				}
+				
+				bounds.X = (bounds.Width - numberSize.Width) / 2f + .5f;
+				bounds.Y += 2f;
+				
+				CALayer badge = new CALayer ();
+				badge.Frame = rect;
+				
+				var imageSize = badge.Frame.Size;
+				
+				// Render the image @x2 for retina people
+				if (UIScreen.MainScreen.Scale == 2) {
+					imageSize = new SizeF (imageSize.Width * 2, imageSize.Height * 2);
+					badge.Frame = new RectangleF (badge.Frame.Location, new SizeF (
+						badge.Frame.Width * 2, badge.Frame.Height * 2));
+					
+					fontSize *= 2;
+					bounds.X = ((bounds.Width * 2) - (numberSize.Width * 2)) / 2f + 1;
+					bounds.Y += 3;
+					bounds.Width *= 2;
+					radius *= 2;
+				}
+				
+				badge.BackgroundColor = color.CGColor;
+				badge.CornerRadius = radius;
+				
+				UIGraphics.BeginImageContext (imageSize);
+				var context = UIGraphics.GetCurrentContext ();
+				
+				context.SaveState ();
+				badge.RenderInContext (context);
+				context.RestoreState ();
+				
+				context.SetBlendMode (CGBlendMode.Clear);
+				text.DrawString (bounds, UIFont.BoldSystemFontOfSize (fontSize), UILineBreakMode.Clip);
+				context.SetBlendMode (CGBlendMode.Normal);
+				
+				var outputImage = UIGraphics.GetImageFromCurrentImageContext ();
+				UIGraphics.EndImageContext ();
+				
+				outputImage.Draw (rect);
+				if (Parent.SelectionStyle == UITableViewCellSelectionStyle.None && (Parent.Highlighted || Parent.Selected) && Shadow) {
+					Layer.CornerRadius = radius;
+					Layer.ShadowOffset = new SizeF (0f, 1f);
+					Layer.ShadowRadius = 1f;
+					Layer.ShadowOpacity = .8f;
+				} else {
+					Layer.CornerRadius = radius;
+					Layer.ShadowOffset = SizeF.Empty;
+					Layer.ShadowRadius = 0f;
+					Layer.ShadowOpacity = 0f;
+				}
+			}
+		}
+		
+		private class BadgeCell : UITableViewCell
+		{
+			private BadgeView badge;
+			
+			public string BadgeText;
+			public bool ShowShadow;
+			public UIColor HighlightColor = UIColor.FromRGBA (1f, 1f, 1f, 1f);
+			public UIColor Color = UIColor.FromRGBA (0.530f, 0.600f, 0.738f, 1f);
+			public float Radius = 4f;
+			
+			public BadgeCell (NSString cellIdentifier) : base(UITableViewCellStyle.Default, cellIdentifier)
+			{
+				badge = new BadgeView (RectangleF.Empty);
+				badge.Parent = this;
+				
+				ContentView.AddSubview (badge);
+				badge.SetNeedsDisplay ();
+			}
+			
+			public override void LayoutSubviews ()
+			{
+				base.LayoutSubviews ();
+				
+				if (!string.IsNullOrEmpty (BadgeText)) {
+					if (Editing)
+						badge.Hidden = true;
+					else
+						badge.Hidden = false;
+					
+					var badgeSize = new NSString (BadgeText).StringSize (UIFont.BoldSystemFontOfSize (11));
+					var badgeFrame = new RectangleF (ContentView.Frame.Width - (badgeSize.Width + 25f),
+					                                (float)Math.Round ((ContentView.Frame.Height - 18f) / 2f),
+					                                badgeSize.Width + 13f, 18f);
+					badge.Shadow = ShowShadow;
+					badge.Frame = badgeFrame;
+					badge.Text = BadgeText;
+					badge.Radius = Radius;
+					
+					float badgeWidth;
+					if (TextLabel.Frame.Right >= badgeFrame.X) {
+						badgeWidth = TextLabel.Frame.Width - badgeFrame.Width - 10;
+						TextLabel.Frame = new RectangleF (TextLabel.Frame.Location, new SizeF (badgeWidth, TextLabel.Frame.Height));
+					}
+					
+					badge.HighlightColor = HighlightColor;
+					badge.Color = Color;
+				} else {
+					badge.Hidden = true;
+				}
+			}
+			
+			public override void SetHighlighted (bool highlighted, bool animated)
+			{
+				base.SetHighlighted (highlighted, animated);
+				badge.SetNeedsDisplay ();
+			}
+		}
 	}
 }
