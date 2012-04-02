33ac6bde06dc0aabe127cfc7299a56c1352ee5b8
Sebastien Pouliot <sebastien@xamarin.com>
2012-04-02 13:58:02 -0400

    Revert "Don't assume that there is a Source value"
    This reverts commit e000770f80355b0c22969cd6f68b4e74f61f44c8.

--- MonoTouch.Dialog/DialogViewController.cs
+++ MonoTouch.Dialog/DialogViewController.cs
@@ -621,14 +621,11 @@ namespace MonoTouch.Dialog
 			// on an implementation detail; the base SectionIndexTitles() method to be implementated
 			// in UITableViewSource.
 			
-			if(tableView.Source != null)
-			{
-				MethodInfo m = tableView.Source.GetType().GetMethod("SectionIndexTitles", BindingFlags.Instance | BindingFlags.Public);
+			MethodInfo m = tableView.Source.GetType().GetMethod("SectionIndexTitles", BindingFlags.Instance | BindingFlags.Public);
 			
-				if (!m.DeclaringType.Name.Equals(typeof(UITableViewSource).Name)) {
-					if (tableView.Source.SectionIndexTitles(tableView).Length > 0) {
-						return true;
-					}
+			if (!m.DeclaringType.Name.Equals(typeof(UITableViewSource).Name)) {
+				if (tableView.Source.SectionIndexTitles(tableView).Length > 0) {
+					return true;
 				}
 			}
 						
