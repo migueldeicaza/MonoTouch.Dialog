cf3023eda855cd3a520a98d4477734809288efd2
Rolf Bjarne Kvinge <rolf@xamarin.com>
2012-04-11 15:18:11 +0200

    Clear the background of StyledStringElements properly when cells are reused. Fixes #3976.

--- MonoTouch.Dialog/Elements.cs
+++ MonoTouch.Dialog/Elements.cs
@@ -829,8 +829,7 @@ namespace MonoTouch.Dialog
 				cell.DetailTextLabel.Text = Value == null ? "" : Value;
 			
 			if (extraInfo == null){
-				cell.ContentView.BackgroundColor = null;
-				tl.BackgroundColor = null;
+				ClearBackground (cell);
 			} else {
 				var imgView = cell.ImageView;
 				UIImage img;
