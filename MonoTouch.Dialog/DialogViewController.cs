c251761616447a93c9d0e8d0d1ac2067cf40c722
Paul Donovan <pdonovan@cosmos21.com.au>
2012-11-27 12:03:50 +1100

    Clear the text of the searchBar if the user taps the search's Cancel button.

--- MonoTouch.Dialog/DialogViewController.cs
+++ MonoTouch.Dialog/DialogViewController.cs
@@ -279,6 +279,7 @@ namespace MonoTouch.Dialog
 			public override void CancelButtonClicked (UISearchBar searchBar)
 			{
 				searchBar.ShowsCancelButton = false;
+				container.searchBar.Text = "";
 				container.FinishSearch ();
 				searchBar.ResignFirstResponder ();
 			}
